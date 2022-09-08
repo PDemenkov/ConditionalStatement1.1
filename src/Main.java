9251497ff3006e11785676e1d6d67716419eba47
PDemenkov <guapnik@gmail.com>
2022-09-08 18:37:59 +0300

    Задача 2+3

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,32 @@ public class Main {
         if (age<=18){
             System.out.println("Совершенолетие еще не наступило");
         }
+
+        System.out.println("Задание 2");
+        int age1 = 7;
+        int age2 = 18;
+        int age3 = 24;
+        if (age1>=7){
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age2>=18){
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (age3>=24){
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+
+        System.out.println("Задание 3");
+        int capacityCarriage =102;
+        int sittingPlace =60;
+        if (capacityCarriage>=102){
+            System.out.println("Вагон забит");
+        }
+        if (sittingPlace<60){
+            System.out.println("Есть сидячие места");
+        }
+        if (sittingPlace>=60){
+            System.out.println("Только стоячие места");
+        }
     }
 }
