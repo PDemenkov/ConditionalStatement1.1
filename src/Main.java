c655a4718d5a0306361e01a718a10e62ea17aca1
PDemenkov <guapnik@gmail.com>
2022-09-08 18:20:14 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int age = 18;
+        if (age>=18) {
+            System.out.println("Поздравляем с совершенолетием!");
+        }
+        if (age<=18){
+            System.out.println("Совершенолетие еще не наступило");
+        }
+    }
+}
